f870d426413beb8e24efbf00184b7118aba71d92
panecillouis <paula54carrascosa85@gmail.com>
2025-03-05 15:58:23 +0100

    Added comments to the 1.cpp and 3.py files

--- Buenas_Prácticas/Ejercicio1/1.cpp
+++ Buenas_Prácticas/Ejercicio1/1.cpp
@@ -10,9 +10,9 @@ int main() {
     int ancho=0,alto=0;  // A large number
     printf("Calculadora del area de un rectangulo.\n");
     printf("Introduce el ancho.\n");
-    scanf("%d",&ancho);
+    scanf("%d",&ancho); // CWE-20 (Improper Input Validation)
     printf("Introduce el alto.\n");
-    scanf("%d",&alto);
+    scanf("%d",&alto); // CWE-20 (Improper Input Validation)
     calculate_area(ancho, alto);
     return 0;
 }
--- Buenas_Prácticas/Ejercicio3/3.py
+++ Buenas_Prácticas/Ejercicio3/3.py
@@ -9,13 +9,16 @@ Application to create users and download games of players
 
 app = Flask(__name__)
 def is_valid_password(pwd):
-    if not (10 <= len(pwd) <= 15):
+    if not (10 <= len(pwd) <= 15): #CWE-521 Weak Password Requirements
         return False
-    digit_count = sum(c.isdigit() for c in pwd)
-    return digit_count >= 2
+    digit_count = sum(c.isdigit() for c in pwd) #CWE-521 Weak Password Requirements
+    return digit_count >= 2 #CWE-521 Weak Password Requirements
 
 def create_user(username, password):
-    hashed_password = hashlib.md5(password.encode()).hexdigest()
+    #CWE-327 Use of a Broken or Risky Cryptographic Algorithm
+    #CWE-916 Use of Password Hash With Insufficient Computational Effort
+    hashed_password = hashlib.md5(password.encode()).hexdigest() 
+    
     conn = sqlite3.connect(os.getenv("DB_NAME"))
     cursor = conn.cursor()
     query = "INSERT INTO users (username, password) VALUES (?, ?)"
@@ -25,10 +28,11 @@ def create_user(username, password):
 
 @app.route("/")
 def index():
-    user = request.args.get("id", "1")
-    pwd = request.args.get("pwd", "")
+    user = request.args.get("id", "1")     #CWE-639 Authorization Bypass Through User-Controlled Key
 
-    if is_valid_password(pwd) and user.isalpha():
+    pwd = request.args.get("pwd", "")
+    
+    if is_valid_password(pwd) and user.isalpha(): #CWE-20 Improper Input Validation
         create_user(user,pwd)
 
     html_response = f"""
@@ -43,7 +47,7 @@ def download_games(game_id):
     file_name = f"{game_id}.pgn"
     if not os.path.isfile(file_name):
         abort(404, "File not found")
-    return send_file(file_name, as_attachment=True)
+    return send_file(file_name, as_attachment=True) #CWE-35 Path Traversal
 
 if __name__ == "__main__":
     app.run(debug=False)
